0331a9b5dc6191ac7f1325902736c3a183a581c9
Ricardo Samuel Coronel Rojas <ricardocoronel863@gmail.com>
2026-09-15 08:59:53 -0500

    Restauracion de la clase principal en la ruta correcta

--- evidencias/src/main/java/com/portafolio/evidencias/EvidenciasApplication.java
+++ evidencias/src/main/java/com/portafolio/evidencias/EvidenciasApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EvidenciasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EvidenciasApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EvidenciasApplication.class, args);
+    }
 }
